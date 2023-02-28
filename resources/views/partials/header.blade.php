264dfa9b54e668b35a25e78a4b0931d729ca8160
Aurora-Bizzo <aurora.bizzozero98@gmail.com>
2023-02-28 01:39:41 +0100

    first header model

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,35 @@
+<header>
+    <nav class="p-3 bg-black">
+        <div class="container">
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-6">
+                    <div>
+                        <ul class="navbar-nav mx-auto d-flex flex-row justify-content-between align-items-center">
+                            <li class="nav-li">
+                                <a class="nav-li mx-2" href="#">Home</a>
+                            </li>
+                            <li class="nav-li">
+                                <a class="nav-li mx-2" href="#">Movies</a> 
+                            </li>
+                            <li class="nav-i display-4">
+                                <div class="d-flex flex-column align-items-center">
+                                    <h6 class="m-0">MOVIE</h6>
+                                    <i class="fa-solid fa-film"></i>
+                                    <h4>STUDIOS</h4>
+                                </div>
+                            </li>
+                            <li class="nav-li">
+                                <a class="nav-li mx-2" href="#">Series</a>
+                            </li>
+                            <li class="nav-li">
+                                <a class="nav-li mx-2" href="#">About Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-3"></div>
+            </div>
+        </div>
+    </nav>
+</header>
